6526935768eb00744d90871c2af8bb0f4a1372c8
Nikita Reutov <liljollier@gmail.com>
2020-07-22 06:50:37 +0300

    add draggable timeline

--- src/app/video-player/video-player.component.ts
+++ src/app/video-player/video-player.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { combineLatest, fromEvent, Observable } from 'rxjs';
-import { map, } from 'rxjs/operators';
+import { combineLatest, fromEvent, merge, Observable, Subject } from 'rxjs';
+import { map, tap, } from 'rxjs/operators';
 
 @Component({
   selector: 'app-video-player',
@@ -10,15 +10,15 @@ import { map, } from 'rxjs/operators';
 export class VideoPlayerComponent implements AfterViewInit {
 
   @ViewChild('video', {static: false}) video: ElementRef;
+  @ViewChild('timeline', {static: false}) timelineEl: ElementRef;
 
   videoSource = 'assets/video/1.mov';
+  playState = false;
 
   videoDuration$: Observable<number>;
   currentTime$: Observable<number>;
   timeline$: Observable<string>;
-
-  playState = false;
-
+  timelineChangeSub$: Subject<Event> = new Subject<Event>();
 
   get videoElem() {
     return (this.video ? this.video.nativeElement : null) as HTMLVideoElement;
@@ -37,11 +37,26 @@ export class VideoPlayerComponent implements AfterViewInit {
       map(() => this.videoElem !== null ? Number(this.videoElem.currentTime.toFixed(0)) : 0),
     );
 
-    this.timeline$ = combineLatest(this.currentTime$, this.videoDuration$)
+
+    const timelineDuration$ = combineLatest(this.currentTime$, this.videoDuration$)
       .pipe(
         map(([current, full]) => {
-          return ((full / 100) * current).toFixed(2) + '%';
+          return (current / (full / 100)).toFixed(2);
         })
+      );
+
+    const timelineChange$ = this.timelineChangeSub$
+      .pipe(
+        map((event: any) => {
+          const percent = (event.clientX / (this.timelineEl.nativeElement.getBoundingClientRect().right / 100));
+          this.videoElem.currentTime = Number(((this.videoElem.duration / 100) * percent).toFixed(0));
+          return percent.toFixed(2);
+        }),
+      );
+
+    this.timeline$ = merge(timelineDuration$, timelineChange$)
+      .pipe(
+        map(val => val + '%')
       );
   }
 
@@ -59,6 +74,10 @@ export class VideoPlayerComponent implements AfterViewInit {
     this.videoElem.pause();
     this.videoElem.currentTime = 0;
     this.playState = false;
+  }
+
+  timelineChange(event: Event) {
+    this.timelineChangeSub$.next(event);
   }
 
 }
