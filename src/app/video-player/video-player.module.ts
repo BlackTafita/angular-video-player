713089959c13426739064eedfb3f4a426a515ca7
Nikita Reutov <liljollier@gmail.com>
2020-07-29 05:39:51 +0300

    add base functionality for volume

--- src/app/video-player/video-player.module.ts
+++ src/app/video-player/video-player.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { VideoPlayerComponent } from './video-player.component';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   declarations: [VideoPlayerComponent],
   exports: [VideoPlayerComponent],
